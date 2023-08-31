edf37d12953216cac4ec042f83d101664d78c5de
xdong <hy44jt@gmail.com>
2023-08-31 17:26:09 +0900

    stream

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, Sse } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Observable } from 'rxjs';
+import { exec } from 'child_process';
+import { readFileSync } from 'fs';
 
 @Controller()
 export class AppController {
@@ -23,6 +25,26 @@ export class AppController {
       setTimeout(() => {
         observer.next({ data: { msg: 'ccc' } });
       }, 5000);
+    });
+  }
+
+  @Sse('stream2')
+  stream2() {
+    const childProcess = exec('tail -f ./log');
+
+    return new Observable((observer) => {
+      childProcess.stdout.on('data', (msg) => {
+        console.log('msg', msg);
+        observer.next({ data: { msg: msg.toString() } });
+      });
+    });
+  }
+
+  @Sse('stream3')
+  stream3() {
+    return new Observable((observer) => {
+      const json = readFileSync('./package.json').toJSON();
+      observer.next({ data: { msg: json } });
     });
   }
 }
